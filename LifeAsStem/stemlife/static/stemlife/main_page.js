04d544c18cd2bdd9ecc699e0ce84574239851d81
Siddhi Bavare <siddhibavare@gmail.com>
2024-01-28 03:33:27 -0600

    iteraation through json successful!

--- LifeAsStem/stemlife/static/stemlife/main_page.js
+++ LifeAsStem/stemlife/static/stemlife/main_page.js
@@ -5,15 +5,42 @@ heightOfPlant = 0
 document.addEventListener('DOMContentLoaded', ()=>{
     let btn = document.getElementById('new');
     btn.addEventListener("click", (evt)=>{ 
-        playerAge += 1
+        
         document.getElementById("playerScore").innerHTML = "Age: " +playerAge
         console.info("playerAge: " + playerAge)
         document.getElementById("computerInput").innerHTML.visibility = "visible"
-        document.getElementById("computerInput").innerHTML = "Question: How tall is the plant?"
+        
+        fetch('/questions')
+        .then(response => response.json())
+        .then(question => {
+            question.forEach(element => {
+                currentQS = element
+                if (currentQS.age == playerAge){
+                    console.log(currentQS)
+                    document.getElementById("computerInput").innerHTML = currentQS.text
+                    
+                    if (currentQS.answer1.length>1){
+                        document.getElementById("ans_one").innerHTML = currentQS.answer1
+                    }
+                    if (currentQS.answer2.length>1){
+                        document.getElementById("ans_two").innerHTML = currentQS.answer2
+                    }
+                    if (currentQS.answer3.length>1){
+                        document.getElementById("ans_three").innerHTML = currentQS.answer3
+                    }
+                    if (currentQS.answer4.length>1){
+                        document.getElementById("ans_four").innerHTML = currentQS.answer4
+                    }
+                }
+            })
+                    });
+        playerAge+=1
+
+    })
         //if btn and all the questions are answered, increase the age again
         //if not, ask another question from bank of questions
         
-    })
+    
 })
 
 
