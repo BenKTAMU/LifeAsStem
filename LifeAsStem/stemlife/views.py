26d9fac95a2f88c6aba624baeeafbcd960476eb6
BenKTAMU <145497372+BenKTAMU@users.noreply.github.com>
2024-01-27 21:08:49 -0600

    fixed stuff?

--- LifeAsStem/stemlife/views.py
+++ LifeAsStem/stemlife/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db import IntegrityError
 from django.urls import reverse
 from .models import User, Questions
-from django.contrib.auth import authenticate, login, logout, questions
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
